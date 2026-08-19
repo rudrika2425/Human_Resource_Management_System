4e7bdd2309f6e427bad5559f08d8d3dbd8a225f6
Rudrika Raghav <116345691+rudrika2425@users.noreply.github.com>
2026-08-19 23:58:15 +0530

    frontend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { storage } from '../utils/storage';
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || 'https://humanresourcemanagementsystem-production.up.railway.app/';
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'https://humanresourcemanagementsystem-production.up.railway.app';
 
 export const api = axios.create({
   baseURL,
